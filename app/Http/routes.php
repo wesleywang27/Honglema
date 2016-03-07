dece5dfe6e8e614b7d469f4419a18eb2961692ff
黄雅冠 <huangyg11@gmail.com>
2016-03-07 11:32:14 +0800

    1次加载10个

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,12 +14,12 @@ use App\Models\Picture;
 */
 
 Route::get('/celebrities/list', function () {
-    $celebrities = Celebrity::with('pictures')->paginate(6);
+    $celebrities = Celebrity::with('pictures')->paginate(10);
     return view('celebrities', ['celebrities' => $celebrities]);
 });
 
 Route::get('/celebrities/list.json', function () {
-    $celebrities = Celebrity::with('pictures')->paginate(6);
+    $celebrities = Celebrity::with('pictures')->paginate(10);
     return $celebrities;
 });
 
