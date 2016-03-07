d4ab29c57139f216ea757b599cfc5b305b55d325
sflovely <469009092@qq.com>
2016-03-07 16:30:08 +0800

    新版瀑布流

--- resources/views/celebrities.blade.php
+++ resources/views/celebrities.blade.php
@@ -24,6 +24,8 @@
          抛开瀑布流布局各种乱七八糟的算法，基于masonry的瀑布流，很是简单的，而且通过扩展animate,能实现瀑布流布局的晃动、弹球等效果。
          masonry还有很多参数我这里注解了常用的参数
          */
+        var curruntPage = 1;
+        var flag = 1;
         $(function(){
             /*瀑布流开始*/
             var container = $('.waterfull ul');
@@ -47,7 +49,7 @@
             });
             container.imagesLoaded(function(){
                 container.masonry({
-                    columnWidth: 10,
+                    columnWidth: 2,
                     itemSelector : '.item',
                     isFitWidth: true,//是否根据浏览器窗口大小自动适应默认false
                     isAnimated: true,//是否采用jquery动画进行重拍版
@@ -61,10 +63,22 @@
                 });
             });
             /*模拟从后台获取到的数据*/
-            var sqlJson=[{'title':'瀑布流其实就是几个函数的事！','intro':'爆料，苏亚雷斯又咬人啦，C罗哪有内马尔帅，梅西今年要不夺冠，你就去泰国吧，老子买了阿根廷赢得彩票，输了就去不成了。','src':'http://aliv.weipai.cn/honglema/56d7c2460cf29739e653ce3c.jpeg','writer':'志强不息','date':'2小时前','looked':321},{'title':'瀑布流其实就是几个函数的事！','intro':'爆料了，苏亚雷斯又咬人啦，C罗哪有内马尔帅，梅西今年要不夺冠，你就去泰国吧，老子买了阿根廷赢得彩票，输了就去不成了。','src':'http://aliv.weipai.cn/honglema/56d7c2460cf29739e653ce3c.jpeg','writer':'志强不息','date':'2小时前','looked':321},{'title':'瀑布流其实就是几个函数的事！','intro':'爆料了，苏亚雷斯又咬人啦，C罗哪有内马尔帅，梅西今年要不夺冠，你就去泰国吧，老子买了阿根廷赢得彩票，输了就去不成了。','src':'http://aliv.weipai.cn/honglema/56d7c2460cf29739e653ce3c.jpeg','writer':'志强不息','date':'2小时前','looked':321},{'title':'瀑布流其实就是几个函数的事！','intro':'爆料了，苏亚雷斯又咬人啦，C罗哪有内马尔帅，梅西今年要不夺冠，你就去泰国吧，老子买了阿根廷赢得彩票，输了就去不成了。','src':'http://aliv.weipai.cn/honglema/56d7c2460cf29739e653ce3c.jpeg','writer':'志强不息','date':'2小时前','looked':321}];
+            var sqlJson = null;
             /*本应该通过ajax从后台请求过来类似sqljson的数据然后，便利，进行填充，这里我们用sqlJson来模拟一下数据*/
             $(window).scroll(function(){
                 if(!loading.data("on")) return;
+                if(flag == 1){
+                    flag = 0;
+                    $.ajax({
+                        url: '/celebrities/list.json',
+                        type: 'GET',
+                        data:{'page': curruntPage + 1 },
+                        success: function(data){
+                            sqlJson = data.data;
+                            curruntPage = data.current_page;
+                        }
+                    });
+                }
                 // 计算所有瀑布流块中距离顶部最大，进而在滚动条滚动时，来进行ajax请求，方法很多这里只列举最简单一种，最易理解一种
                 var itemNum=$('#waterfull').find('.item').length;
                 var itemArr=[];
@@ -77,30 +91,31 @@
                     loading.data("on",false).fadeIn(800);
                     (function(sqlJson){
                         /*这里会根据后台返回的数据来判断是否你进行分页或者数据加载完毕这里假设大于30就不在加载数据*/
-                        if(itemNum>30){
+                        if(JSON.stringify(sqlJson)=='[]'){
                             loading.text('就有这么多了！');
                         }else{
                             var html="";
                             for(var i in sqlJson){
-                                html+="<li class='item'><a href='####' class='a-img'><img src='"+sqlJson[i].src+"'></a>";
-                                html+="<h2 class='li-title'>"+sqlJson[i].title+"</h2>";
-                                html+="<p class='description'>"+sqlJson[i].intro+"</p><div class='qianm clearfloat'>";
-                                html+="<span class='sp1'><b>"+sqlJson[i].looked+"</b>浏览</span>";
-                                html+="<span class='sp2'>"+sqlJson[i].writer+"</span><span class='sp3'>"+sqlJson[i].date+"&nbsp;By</span></div></li>";
+                                html+="<li class='item'><a href='http://m.honglema.com/celebrity/" + sqlJson[i].id +"' class='a-img'><img src='"+(sqlJson[i].pictures)[1].url+"'></a>";
+                                html+="<h2 class='li-title'>"+sqlJson[i].nickname+"</h2>";
+//                                html+="<p class='description'>"+sqlJson[i].intro+"</p><div class='qianm clearfloat'>";
+                                html+="<div class='qianm clearfloat'><span class='sp1'>粉丝<b>"+sqlJson[i].total_fans_num+"</b></span></div>";
+//                                html+="<span class='sp2'>"+sqlJson[i].writer+"</span><span class='sp3'>"+sqlJson[i].date+"&nbsp;By</span></div></li>";
                             }
                             /*模拟ajax请求数据时延时800毫秒*/
                             var time=setTimeout(function(){
                                 $(html).find('img').each(function(index){
                                     loadImage($(this).attr('src'));
-                                })
-                                var $newElems = $(html).css({ opacity: 0}).appendTo(container);
-                                $newElems.imagesLoaded(function(){
-                                    $newElems.animate({ opacity: 1},800);
-                                    container.masonry( 'appended', $newElems,true);
-                                    loading.data("on",true).fadeOut();
-                                    clearTimeout(time);
                                 });
-                            },800)
+                            var $newElems = $(html).css({ opacity: 0}).appendTo(container);
+                            $newElems.imagesLoaded(function(){
+                                $newElems.animate({ opacity: 1},800);
+                                container.masonry( 'appended', $newElems,true);
+                                loading.data("on",true).fadeOut();
+                                clearTimeout(time);
+                            });
+                            },800);
+                            flag = 1;
                         }
                     })(sqlJson);
                 }
@@ -116,7 +131,7 @@
                     return img.src;
                 };
             };
-            loadImage('./images/one.jpeg',test());
+            // loadImage('./images/one.jpeg',test());
             /*item hover效果*/
             // var rbgB=['#71D3F5','#F0C179','#F28386','#8BD38B'];
             // $('#waterfull').on('mouseover','.item',function(){
@@ -134,23 +149,32 @@
     <!-- 瀑布流样式开始 -->
     <div class="waterfull clearfloat" id="waterfull">
         <ul>
+            @if (count($celebrities) > 0)
+            @foreach ($celebrities as $celebrity)
             <li class="item">
-                <a href="####" class="a-img">
-                    <img src="http://aliv.weipai.cn/honglema/56d7c2460cf29739e653ce3c.jpeg" alt="">
+                <a href="{{ url('/celebrity/'.$celebrity->id) }}" class="a-img">
+                    @foreach ( $celebrity->pictures as $picture)
+                    @if (str_contains($picture, 'jpg') || str_contains($picture, 'jpeg'))
+                    <img src="{{ $picture->url }}" alt="">
+                    @break;
+                    @endif
+                    @endforeach
                 </a>
-                <h2 class="li-title" title="手把手教你用css3来创建loading动画(二)">手把手教你用css3来创建loading动画(二)</h2>
-                <p class="description">HTML 5以前的标准由于考虑到浏览器安全问题并不允许直接跨域通信并不允许直接跨域通信并不允许直接跨域通信，于...</p>
+                <h2 class="li-title" title="{{ $celebrity->nickname }}">{{ $celebrity->nickname }}</h2>
+<!--                <p class="description">HTML 5以前的标准由于考虑到浏览器安全问题并不允许直接跨域通信并不允许直接跨域通信并不允许直接跨域通信，于...</p>-->
                 <div class="qianm clearfloat">
-                    <span class="sp1"><b>688</b>浏览</span>
-                    <span class="sp2">志强不息</span>
-                    <span class="sp3">2小时前&nbsp;By</span>
+                    <span class="sp1">粉丝<b>{{ $celebrity->total_fans_num }}</b></span>
+<!--                    <span class="sp2">志强不息</span>-->
+<!--                    <span class="sp3">2小时前&nbsp;By</span>-->
                 </div>
             </li>
+            @endforeach
+            @endif
         </ul>
     </div>
     <!-- loading按钮自己通过样式调整 -->
-    <div id="imloading" style="width:150px;height:30px;line-height:30px;font-size:16px;text-align:center;border-radius:3px;opacity:0.7;background:#000;margin:10px auto 30px;color:#fff;display:none">
-        I'm Loading.....
+    <div id='imloading' style='width:150px;height:30px;line-height:30px;font-size:16px;text-align:center;border-radius:3px;opacity:0.7;background:#000;margin:10px auto 30px;color:#fff;display:none;'>
+        正在加载...
     </div>
 </div>
 </body>
