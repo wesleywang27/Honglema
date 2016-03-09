b2c8c525678dac934336f07ca615ad3c5d29a66b
sflovely <469009092@qq.com>
2016-03-09 16:06:24 +0800

    调整柱状图间距

--- public/js/Chart.js
+++ public/js/Chart.js
@@ -2284,7 +2284,7 @@
 		barStrokeWidth : 2,
 
 		//Number - Spacing between each of the X value sets
-		barValueSpacing : 20,
+		barValueSpacing : 5,
 
 		//Number - Spacing between data sets within X values
 		barDatasetSpacing : 1,
