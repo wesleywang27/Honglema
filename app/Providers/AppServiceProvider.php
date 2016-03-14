20d1165764e92bfac6f7ea8f1a7572a30d7437c3
黄雅冠 <huangyg11@gmail.com>
2016-03-14 15:08:11 +0800

    disable wechat temporarily

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share('wechat', \Wechat::js());
+        //view()->share('wechat', \Wechat::js());
     }
 
     /**
