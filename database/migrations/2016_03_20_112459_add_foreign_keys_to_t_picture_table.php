e49d08c89e027b0757718118b28196f08355dc00
黄雅冠 <huangyg11@gmail.com>
2016-04-13 12:59:55 +0800

    修正表名

--- database/migrations/2016_03_20_112459_add_foreign_keys_to_t_picture_table.php
+++ database/migrations/2016_03_20_112459_add_foreign_keys_to_t_picture_table.php
@@ -14,7 +14,7 @@ class AddForeignKeysToTPictureTable extends Migration {
 	{
 		Schema::table('picture', function(Blueprint $table)
 		{
-			$table->foreign('uid', 'picture_ibfk_1')->references('id')->on('t_celebrity')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+			$table->foreign('uid', 't_picture_ibfk_1')->references('id')->on('t_celebrity')->onUpdate('RESTRICT')->onDelete('RESTRICT');
 		});
 	}
 
@@ -28,7 +28,7 @@ class AddForeignKeysToTPictureTable extends Migration {
 	{
 		Schema::table('picture', function(Blueprint $table)
 		{
-			$table->dropForeign('picture_ibfk_1');
+			$table->dropForeign('t_picture_ibfk_1');
 		});
 	}
 
