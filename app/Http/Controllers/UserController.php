63de3531bf17dd6b8087c62841c4c5416b0e6d56
EricWang427 <744731059@qq.com>
2016-04-19 11:25:02 +0800

    UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: 王得屹
+ * Date: 2016/4/19
+ * Time: 11:23
+ */
+namespace App\Http\Controllers;
+
+use Validator;
+use Illuminate\Support\Facades\Input;
+use App\User;
