f42be5cfda147d8e4f35a796f0e5f63e899a528b
sflovely <469009092@qq.com>
2016-04-21 23:08:15 +0800

    修改标题

--- resources/views/celebrities.blade.php
+++ resources/views/celebrities.blade.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no" />
     <meta name="format-detection" content="telephone=no" />
-    <title>index</title>
+    <title>热门广场</title>
     <link rel="stylesheet" type="text/css" href="{{URL::asset('css/normalize.css')}}">
     <link rel="stylesheet" type="text/css" href="{{URL::asset('css/main.css')}}">
     <style>
