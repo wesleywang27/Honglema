5201dbda7a0da1f4c4fa7fe3e00c0ae6e55a0db1
EricWang427 <744731059@qq.com>
2016-04-21 14:32:40 +0800

    品牌商导出excel模块

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -9,7 +9,51 @@ use App\Models\Factory;
 use App\Models\Stall;
 
 class ExcelController extends Controller{
-    //档口信息导出Excel
+    public function exportBrand(){
+        $info = Brand::all();
+        foreach ($info as $key => $value){
+            if($value['factory'] == 1)
+                $factory = '是';
+            else
+                $factory = '否';
+            if($value['design'] == 1)
+                $design = '是';
+            else
+                $design = '否';
+            if($value['refund'] == 1)
+                $refund = '是';
+            else
+                $refund = '否';
+            $export[] = array(
+                '用户ID' => $value['brand_id'],
+                '用户名' => $value['username'],
+                '手机号码' => $value['mobile'],
+                '微信号' => $value['weixinNo'],
+                '职位' => $value['title'],
+                '公司名称' => $value['company'],
+                '公司地址' => $value['country'].$value['province'].$value['city'].$value['region'].$value['address'],
+                '品牌名称' => $value['brand'],
+                '2015年线上销售额' => $value['sales'],
+                '类目' => $value['category'],
+                '是否自有工厂' => $factory,
+                '厂房面积' => $value['factorySize'],
+                '是否有设计团队' => $design,
+                '主营产品' => $value['product'],
+                '客单价' => $value['price'],
+                '商品风格' => $value['style'],
+                '客户人群定位' => $value['customPosition'],
+                '客户年龄段' => $value['customAge'],
+                '是否支持退换' => $refund,
+                '备注' => $value['description']
+            );
+        }
+        Excel::create('品牌商信息汇总',function($excel) use ($export){
+            $excel->sheet('品牌商信息', function($sheet) use ($export){
+                $sheet->fromArray($export);
+            });
+        })->export('xls');
+    }
+    //设计师信息导出Excel
     public function exportDesigner(){
         $info = Designer::all();
         foreach ($info as $key => $value){
@@ -43,6 +87,7 @@ class ExcelController extends Controller{
             });
         })->export('xls');
     }
+    //档口信息导出Excel
     public function exportStall(){
         $info = Stall::all();
         foreach ($info as $key => $value){
