f729c8ccbdc2bf7018cd81597fa9220647453aa0
EricWang427 <744731059@qq.com>
2016-04-21 11:56:53 +0800

    添加excel导出模块

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -3,21 +3,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Maatwebsite\Excel\Facades\Excel;
+use App\Models\Brand;
+use App\Models\Designer;
+use App\Models\Factory;
+use App\Models\Stall;
+
 class ExcelController extends Controller{
     //Excel文件导出功能
     public function exportStall(){
-        $cellData = [
-            ['学号','姓名','成绩'],
-            ['10001','AAAAA','99'],
-            ['10002','BBBBB','92'],
-            ['10003','CCCCC','95'],
-            ['10004','DDDDD','89'],
-            ['10005','EEEEE','96'],
-        ];
-        Excel::create('学生成绩',function($excel) use ($cellData){
-            $excel->sheet('score', function($sheet) use ($cellData){
-                $sheet->rows($cellData);
+        $info = Stall::all();
+        foreach ($info as $key => $value){
+            $export[] = array(
+                '用户ID' => $value['stall_id'],
+                '用户名' => $value['username'],
+                '手机号码' => $value['mobile'],
+                '微信号' => $value['weixinNo'],
+                '职位' => $value['title'],
+                '档口名称' => $value['stallName'],
+                '档口号' => $value['stallNum'],
+                '档口地址(杭州/广州)' => $value['city'].$value['stall'],
+                '档口地址(其它地区)' => $value['country'].$value['province'].$value['stallCity'].$value['region'].$value['address'],
+                '档口服装风格' => $value['style'],
+                '档口类目' => $value['category'],
+                '是否支持一件代发' => $value['shipmentOK'],
+            );
+        }
+        Excel::create('档口商家信息汇总',function($excel) use ($export){
+            $excel->sheet('档口商家信息', function($sheet) use ($export){
+                $sheet->fromArray($export);
             });
-        })->download('xls');
+        })->export('xls');
     }
 }
