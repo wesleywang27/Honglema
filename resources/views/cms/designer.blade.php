4b8c754bbc629906277cbc93762b7f4b7575f56b
EricWang427 <744731059@qq.com>
2016-04-21 14:04:27 +0800

    设计师导出excel模块

--- resources/views/cms/designer.blade.php
+++ resources/views/cms/designer.blade.php
@@ -53,4 +53,5 @@
     <a class="{{ ($designers->currentPage() == $designers->lastPage()) ? ' disabled' : '' }}" href="{{ $designers->url($designers->currentPage()+1) }}" >后一页</a>
     @endif
 </aside>
+<a href="{{ url('/cms/designer_export') }}" style="float: right;" ><input type="button" value="导出Excel" class="link_btn"/></a>
 @endsection
