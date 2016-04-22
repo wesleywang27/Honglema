48433472601f82cb03a66469074b60efd9dcd82d
EricWang427 <744731059@qq.com>
2016-04-22 12:12:28 +0800

    修改返回链接

--- resources/views/cms/designer_info.blade.php
+++ resources/views/cms/designer_info.blade.php
@@ -58,7 +58,7 @@
                 <td colspan="2"><textarea name="description" style="width: 98%; height: 100px; margin-top: -18px; margin-bottom: -20px;" readonly>{{ $designer->description}}</textarea></td>
             </tr>
         </table>
-        <a href="/cms/designer" style=" margin-left:85%;"><input type="button" value="返回" class="link_btn"/></a>
+        <a href="javascript:history.go(-1);" style=" margin-left:85%;"><input type="button" value="返回" class="link_btn"/></a>
         <!--<input type="button" value="修改" class="link_btn"/>-->
         <a href="{{URL::action('CMSController@deleteDesigner', ['id' => $designer->designer_id]) }}" onclick="return confirm('确定要删除吗？')"><input type="button" value="删除" class="link_btn"/></a>
     </div>
@@ -71,7 +71,7 @@
             </ul>
             <div class="clear"></div>
         </div>
-        <a href="/cms/designer" style=" margin-left:90%;"><input type="button" value="返回" class="link_btn"/></a>
+        <a href="javascript:history.go(-1);" style=" margin-left:90%;"><input type="button" value="返回" class="link_btn"/></a>
         <script src="/js/jquery-1.9.1.min.js"></script>
         <script src="/js/zoom.min.js"></script>
     </div>
