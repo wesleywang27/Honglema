9c24630d4030d895d634d09921948d9610c3bc75
EricWang427 <744731059@qq.com>
2016-05-03 18:04:58 +0800

    界面优化

--- resources/views/cms/index.blade.php
+++ resources/views/cms/index.blade.php
@@ -4,5 +4,11 @@
 <div class="page_title">
     <h2 class="fl">红了吗后台管理系统</h2>
 </div>
-<p style="font-size: 220px; margin-top: 80px; margin-left: 6%; color: #D3D3D3">Welcome</p>
+<p id="welcome" style="margin-top: 80px; text-align: center; color: #D3D3D3">Welcome</p>
+<script>
+    var width;
+    width = (document.body.clientWidth / 10) + 50;
+    width = width + "px";
+    document.getElementById("welcome").style.fontSize = width;
+</script>
 @endsection
