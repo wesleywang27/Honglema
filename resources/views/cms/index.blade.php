5131338f7fd6a6fac95e352dfc1f61e8acabd047
EricWang427 <744731059@qq.com>
2016-05-03 17:14:23 +0800

    界面优化

--- resources/views/cms/index.blade.php
+++ resources/views/cms/index.blade.php
@@ -1,7 +1,8 @@
 @extends('cms.banner')
 
 @section('content')
-<h2><strong style="color:black;">红了吗信息管理系统</strong></h2>
-<div class="page_title" style="margin-top: -30px;"></div>
+<div class="page_title">
+    <h2 class="fl">红了吗后台管理系统</h2>
+</div>
 <p style="font-size: 220px; margin-top: 80px; margin-left: 6%; color: #D3D3D3">Welcome</p>
 @endsection
