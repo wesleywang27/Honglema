c4ab103e31484712b7cf52c50a6a31546ce889f9
王得屹 <744731059@qq.com>
2016-05-17 16:45:16 +0800

    修改访问域名

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -99,128 +99,132 @@ Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
  * 红了吗后台管理系统入口
  */
 
-//登录登出模块入口
-Route::get('/cms/logout', 'CMSController@logout');
+Route::group(['domain' => 'cms.honglema.com'], function() {
 
-Route::post('/cms/login.php','CMSController@login');
+    //登录登出模块入口
+    Route::get('/cms/logout', 'CMSController@logout');
 
-Route::get('/cms/login.php',function () {
-    return view('/cms/login');
-});
+    Route::post('/cms/login.php','CMSController@login');
+
+    Route::get('/cms/login.php',function () {
+        return view('/cms/login');
+    });
 
 //首页入口
-Route::get('/cms/index', "CMSController@index");
+    Route::get('/cms/index', "CMSController@index");
 
-Route::get('/cms/', "CMSController@index");
+    Route::get('/cms/', "CMSController@index");
 
 //访问列表入口
-Route::resource('/cms/factoryList', "CMSController@factory");
+    Route::resource('/cms/factoryList', "CMSController@factory");
 
-Route::resource('/cms/brandList', "CMSController@brand");
+    Route::resource('/cms/brandList', "CMSController@brand");
 
-Route::resource('/cms/designerList', "CMSController@designer");
+    Route::resource('/cms/designerList', "CMSController@designer");
 
-Route::resource('/cms/stallList', "CMSController@stall");
+    Route::resource('/cms/stallList', "CMSController@stall");
 
 //信息详情入口
-Route::get('/cms/factory_info/{id?}',"CMSController@factory_info");
+    Route::get('/cms/factory_info/{id?}',"CMSController@factory_info");
 
-Route::get('/cms/brand_info/{id?}',"CMSController@brand_info");
+    Route::get('/cms/brand_info/{id?}',"CMSController@brand_info");
 
-Route::get('/cms/designer_info/{id?}',"CMSController@designer_info");
+    Route::get('/cms/designer_info/{id?}',"CMSController@designer_info");
 
-Route::get('/cms/stall_info/{id?}',"CMSController@stall_info");
+    Route::get('/cms/stall_info/{id?}',"CMSController@stall_info");
 
 //信息修改入口
-Route::get('/cms/factory_modify/{id?}',"CMSController@modifyFactoryInfo");
+    Route::get('/cms/factory_modify/{id?}',"CMSController@modifyFactoryInfo");
 
-Route::post('/cms/factory_modify/{id?}',"CMSController@updateFactoryInfo");
+    Route::post('/cms/factory_modify/{id?}',"CMSController@updateFactoryInfo");
 
-Route::get('/cms/brand_modify/{id?}',"CMSController@modifyBrandInfo");
+    Route::get('/cms/brand_modify/{id?}',"CMSController@modifyBrandInfo");
 
-Route::post('/cms/brand_modify/{id?}',"CMSController@updateBrandInfo");
+    Route::post('/cms/brand_modify/{id?}',"CMSController@updateBrandInfo");
 
-Route::get('/cms/designer_modify/{id?}',"CMSController@modifyDesignerInfo");
+    Route::get('/cms/designer_modify/{id?}',"CMSController@modifyDesignerInfo");
 
-Route::post('/cms/designer_modify/{id?}', "CMSController@updateDesignerInfo");
+    Route::post('/cms/designer_modify/{id?}', "CMSController@updateDesignerInfo");
 
-Route::get('/cms/stall_modify/{id?}',"CMSController@modifyStallInfo");
+    Route::get('/cms/stall_modify/{id?}',"CMSController@modifyStallInfo");
 
-Route::post('/cms/stall_modify/{id?}',"CMSController@updateStallInfo");
+    Route::post('/cms/stall_modify/{id?}',"CMSController@updateStallInfo");
 
 //信息删除入口
-Route::get('/cms/factory_delete/{id?}',"CMSController@deleteFactory");
+    Route::get('/cms/factory_delete/{id?}',"CMSController@deleteFactory");
 
-Route::get('/cms/brand_delete/{id?}',"CMSController@deleteBrand");
+    Route::get('/cms/brand_delete/{id?}',"CMSController@deleteBrand");
 
-Route::get('/cms/designer_delete/{id?}',"CMSController@deleteDesigner");
+    Route::get('/cms/designer_delete/{id?}',"CMSController@deleteDesigner");
 
-Route::get('/cms/stall_delete/{id?}',"CMSController@deleteStall");
+    Route::get('/cms/stall_delete/{id?}',"CMSController@deleteStall");
 
 //信息查找入口
-Route::get('/cms/factory/{name?}/{category?}', "CMSController@searchFactory");
+    Route::get('/cms/factory/{name?}/{category?}', "CMSController@searchFactory");
 
-Route::get('/cms/brand/{name?}/{category?}',"CMSController@searchBrand");
+    Route::get('/cms/brand/{name?}/{category?}',"CMSController@searchBrand");
 
-Route::get('/cms/designer/{name?}/{category?}',"CMSController@searchDesigner");
+    Route::get('/cms/designer/{name?}/{category?}',"CMSController@searchDesigner");
 
-Route::get('/cms/stall/{name?}/{category?}',"CMSController@searchStall");
+    Route::get('/cms/stall/{name?}/{category?}',"CMSController@searchStall");
 
 //图片修改入口
-Route::post('/cms/factory_updateImg/{id?}',"CMSController@updateFactoryImg");
+    Route::post('/cms/factory_updateImg/{id?}',"CMSController@updateFactoryImg");
 
-Route::post('/cms/brand_updateImg/{id?}',"CMSController@updateBrandImg");
+    Route::post('/cms/brand_updateImg/{id?}',"CMSController@updateBrandImg");
 
-Route::post('/cms/designer_updateImg/{id?}',"CMSController@updateDesignerImg");
+    Route::post('/cms/designer_updateImg/{id?}',"CMSController@updateDesignerImg");
 
-Route::post('/cms/stall_updateImg/{id?}',"CMSController@updateStallImg");
+    Route::post('/cms/stall_updateImg/{id?}',"CMSController@updateStallImg");
 
 //商家添加页
-Route::get('/cms/factory_create',"CMSController@createFactoryIndex");
+    Route::get('/cms/factory_create',"CMSController@createFactoryIndex");
 
-Route::post('/cms/factory_create',"CMSController@createFactory");
+    Route::post('/cms/factory_create',"CMSController@createFactory");
 
-Route::get('/cms/brand_create',"CMSController@createBrandIndex");
+    Route::get('/cms/brand_create',"CMSController@createBrandIndex");
 
-Route::post('/cms/brand_create',"CMSController@createBrand");
+    Route::post('/cms/brand_create',"CMSController@createBrand");
 
-Route::get('/cms/designer_create',"CMSController@createDesignerIndex");
+    Route::get('/cms/designer_create',"CMSController@createDesignerIndex");
 
-Route::post('/cms/designer_create',"CMSController@createDesigner");
+    Route::post('/cms/designer_create',"CMSController@createDesigner");
 
-Route::get('/cms/stall_create',"CMSController@createStallIndex");
+    Route::get('/cms/stall_create',"CMSController@createStallIndex");
 
-Route::post('/cms/stall_create',"CMSController@createStall");
+    Route::post('/cms/stall_create',"CMSController@createStall");
 
 //数据导出入口
-Route::get('/cms/factory_export',"ExcelController@exportFactory");
+    Route::get('/cms/factory_export',"ExcelController@exportFactory");
 
-Route::get('/cms/brand_export',"ExcelController@exportBrand");
+    Route::get('/cms/brand_export',"ExcelController@exportBrand");
 
-Route::get('/cms/designer_export',"ExcelController@exportDesigner");
+    Route::get('/cms/designer_export',"ExcelController@exportDesigner");
 
-Route::get('/cms/stall_export',"ExcelController@exportStall");
+    Route::get('/cms/stall_export',"ExcelController@exportStall");
 
 //用户管理入口
-Route::get('/cms/user',"UserController@index");
+    Route::get('/cms/user',"UserController@index");
 
-Route::post('/cms/user',"UserController@searchUser");
+    Route::post('/cms/user',"UserController@searchUser");
 
-Route::get('/cms/user/{id?}',"UserController@deleteUser");
+    Route::get('/cms/user/{id?}',"UserController@deleteUser");
 
-Route::get('/cms/user_create',"UserController@createUserIndex");
+    Route::get('/cms/user_create',"UserController@createUserIndex");
 
-Route::post('/cms/user_create',"UserController@createUser");
+    Route::post('/cms/user_create',"UserController@createUser");
 
-Route::get('/cms/user_info_confirm',"UserController@user_info_confirm");
+    Route::get('/cms/user_info_confirm',"UserController@user_info_confirm");
 
-Route::get('/cms/user_info',"UserController@user_info");
+    Route::get('/cms/user_info',"UserController@user_info");
 
-Route::post('/cms/user_info',"UserController@updateUser");
+    Route::post('/cms/user_info',"UserController@updateUser");
 
-Route::get('/cms/user_right/{id?}',"UserController@modifyUserRight");
+    Route::get('/cms/user_right/{id?}',"UserController@modifyUserRight");
 
-Route::post('/cms/user_right/{id?}',"UserController@updateUserRight");
+    Route::post('/cms/user_right/{id?}',"UserController@updateUserRight");
 
 //操作日志入口
-Route::get('/cms/logList',"LogController@index");
+    Route::get('/cms/logList',"LogController@index");
+
+});
