4d338a2eeb287eb5f9d0bebc80c4b2c9c2172229
王得屹 <744731059@qq.com>
2016-06-13 17:08:58 +0800

    修改

--- app/Models/Factory.php
+++ app/Models/Factory.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Factory extends Model
-{
+class Factory extends Model{
+    use SoftDeletes;
+
     protected $table = 'factories';
 
     public $timestamps = false;
