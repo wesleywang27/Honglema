1f45e4ae166e0abe80547c700639f3c72503ce2a
王得屹 <744731059@qq.com>
2016-06-14 16:20:53 +0800

    修改

--- database/migrations/2016_06_14_153532_create_commodities_table.php
+++ database/migrations/2016_06_14_153532_create_commodities_table.php
@@ -14,7 +14,7 @@ class CreateCommoditiesTable extends Migration
     {
         Schema::create('commodities', function (Blueprint $table) {
             $table->integer('merchant_id',11);
-            $table->timestamp('create_at');
+            $table->integer('commodity_id',11);
             $table->string('name');
             $table->string('introduction');
             $table->string('url');
