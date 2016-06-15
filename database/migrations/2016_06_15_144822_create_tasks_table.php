0bac1b07780298cd55a5cab9e3a15674b8059d87
王得屹 <744731059@qq.com>
2016-06-15 18:21:52 +0800

    修改

---
+++ database/migrations/2016_06_15_144822_create_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('task_id');
+            $table->integer('activity_id');
+            $table->tinyInteger('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
