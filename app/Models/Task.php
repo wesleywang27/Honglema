57f5f0572b7223bb26362510ea13845328c36748
王得屹 <744731059@qq.com>
2016-06-16 11:36:49 +0800

    添加task

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,5 +10,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model{
-    
+    protected $table = 'tasks';
+
+    public $timestamps = false;
+
+    protected  $fillable = [
+        'activity_id',
+        'status'
+    ];
 }
