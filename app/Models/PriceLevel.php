cf6679bbec635d7fd218dea4028b2673f9fc5d4c
王得屹 <744731059@qq.com>
2016-06-17 12:11:33 +0800

    添加price_level

---
+++ app/Models/PriceLevel.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: 王得屹
+ * Date: 2016/6/17
+ * Time: 11:51
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PriceLevel extends Model{
+    protected $table = 'price_levels';
+
+    public $timestamps = false;
+
+    protected  $fillable = [
+        'level',
+        'price'
+    ];
+}
