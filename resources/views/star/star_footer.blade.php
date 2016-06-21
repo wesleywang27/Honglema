69372d41f7986a93ccb60c52ccadcb35d56e919b
Problemin <problemin@163.com>
2016-06-21 21:48:44 +0800

    在工具栏中删除网红热门广场

--- resources/views/star/star_footer.blade.php
+++ resources/views/star/star_footer.blade.php
@@ -1,8 +1,4 @@
 <nav class="bar bar-tab">
-    <a class="tab-item external active" href="#">
-        <span class="icon icon-star"></span>
-        <span class="tab-label">热门活动</span>
-    </a>
     <a class="tab-item external" href="/star/activityList">
         <span class="icon icon-home"></span>
         <span class="tab-label">活动广场</span>
