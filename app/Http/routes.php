989d96cfc62cb78b6b32f6a5a736f23328e53d3f
He, Hao <hehao@weipai.cn>
2016-06-23 06:46:06 +0800

    Fix a previous sync error in routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -301,6 +301,10 @@ Route::group(['domain' => 'cms.honglema.com'], function() {
 
     Route::get('/didi/ActivityDelete/{activity_id}',"ActivityController@activityDelete");
 
+    Route::get('/didi/ActivityModify/{activity_id?}',"ActivityController@activityModify");
+
+    Route::get('/didi/ActivityChooseOrder/{activity_id?}',"ActivityController@activityChooseOrder");
+    
     Route::get('/didi/ActivityChooseStar/{task_id?}/{star_id}',"ActivityController@activityChooseStar");
 
     Route::get('/didi/ActivityEvaluate/{activity_id?}',"ActivityController@activityEvaluate");
