0cd4a20433d8709c9681d3f1894bc590fb52c273
王得屹 <744731059@qq.com>
2016-06-22 16:32:58 +0800

    滴滴打网红后台 上传活动图片后隐藏

--- resources/views/didi/activity_create.blade.php
+++ resources/views/didi/activity_create.blade.php
@@ -29,9 +29,9 @@
     </div>
     <div class="container">
         <label style="float: left">活动图片：</label>
-        <label class="uploadImg" style="margin-top:10px; margin-left:10px;">
+        <label id="up" class="uploadImg" style="margin-top:10px; margin-left:10px;">
             <span>上传图片</span>
-            <input type="file" id="fileupload" name="imgs[]" type="file" accept="image/jpg,image/jpeg,image/png,image/gif" multiple=""/>
+            <input type="file" id="fileupload" name="imgs[]" type="file" accept="image/jpg,image/jpeg,image/png,image/gif" multiple="" />
         </label>
         <ul class="gallery" id="files" style="display: inline;padding-left: 0;">
         </ul>
@@ -56,7 +56,7 @@
                 var i = 0;
                 $htmls += '<li><img src="'+urls[i]+'" style="width: 80px; height: 80px;"/><input type="hidden" id="itemImage" name="itemImage[]" value="'+urls[i]+'"/></li>';
                 $('#files').append($htmls);
-                $('#uploadImg').hide();
+                document.getElementById('up').style.display='none';
             }
         });
     });
