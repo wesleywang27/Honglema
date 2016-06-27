ce5f8ac57ce8e28cf8a18a007d512b0f1da15f7f
Problemin <problemin@163.com>
2016-06-27 12:15:09 +0800

    提交任务结果确认窗口

--- resources/views/star/star_layouts.blade.php
+++ resources/views/star/star_layouts.blade.php
@@ -577,25 +577,24 @@ $.submmitTaskResult=function(id){
     var playback;
     var views;
     var duration;
-    $.ajax({
-        url: "/star/submitTaskResult",
-        type: "POST",
-        traditional: true,
-        dataType: "JSON",
-        data: {  order_id:id,
-            'playback':playback,
-            'views':views,
-            'duration':duration,
-            'imgdata[]':imgdata,}
-        ,success: function(data) {
-            $.toast("提交成功!",1000);
-        },headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });$.confirm('Are you sure?', function () {
-              $.alert('You clicked Ok button');
-          });
-
+    $.confirm('确认提交任务结果?', function () {
+        $.ajax({
+            url: "/star/submitTaskResult",
+            type: "POST",
+            traditional: true,
+            dataType: "JSON",
+            data: {  order_id:id,
+                'playback':playback,
+                'views':views,
+                'duration':duration,
+                'imgdata[]':imgdata,}
+            ,success: function(data) {
+                $.toast("提交成功!",1000);
+            },headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    });
 }
 
 //网红取消抢单
