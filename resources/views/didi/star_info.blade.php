b16857384b0c8bff2a4ebc3960611e9d459935d5
He, Hao <hehao@weipai.cn>
2016-07-05 17:50:13 +0800

    修改红人信息页尺寸

--- resources/views/didi/star_info.blade.php
+++ resources/views/didi/star_info.blade.php
@@ -16,84 +16,84 @@
         <table border="2" width="95%" height="400" style="margin: 20px;">
             <tr>
                 <th width="10%">红人昵称</th>
-                <td width="13%">{{ $star->name}}</td>
+                <td width="13%" style="text-align:center">{{ $star->name}}</td>
                 <th width="12%">性别</th>
                 @if ($star->sex == 0)
-                <td width="13%">未知</td>
+                <td width="13%" style="text-align:center">未知</td>
                 @elseif ($star->sex == 1)
-                <td width="13%">男</td>
+                <td width="13%" style="text-align:center">男</td>
                 @else
-                <td width="13%">女</td>
+                <td width="13%" style="text-align:center">女</td>
                 @endif
                 <th width="12%">所在地区：</th>
-                <td width="13%">{{ $star->location}}</td>
+                <td width="13%" style="text-align:center">{{ $star->location}}</td>
             </tr>
             <tr>
                 <th>真实姓名</th>
-                <td>{{ $star->real_name}}</td>
+                <td style="text-align:center">{{ $star->real_name}}</td>
                 <th>身份证号</th>
-                <td>{{ $star->ID_number}}</td>
+                <td style="text-align:center">{{ $star->ID_number}}</td>
                 <th>手机号码</th>
-                <td>{{ $star->cellphone}}</td>
+                <td style="text-align:center">{{ $star->cellphone}}</td>
             </tr>
             <tr>
                 <th colspan="6" style="height:10px;"></th>
             </tr>
             <tr>
                 <th>身高</th>
-                <td>{{ $star->height}}</td>
+                <td style="text-align:center">{{ $star->height}}</td>
                 <th>体重</th>
-                <td>{{ $star->weight}}</td>
+                <td style="text-align:center">{{ $star->weight}}</td>
                 <th>罩杯</th>
-                <td>{{ $star->cup}}</td>
+                <td style="text-align:center">{{ $star->cup}}</td>
             </tr>
             <tr>
                 <th>年龄</th>
-                <td>{{ $star->age}}</td>
+                <td style="text-align:center">{{ $star->age}}</td>
                 <th>职业</th>
-                <td>{{ $star->occupation}}</td>
+                <td style="text-align:center">{{ $star->occupation}}</td>
                 <th>学历</th>
-                <td>{{ $star->education}}</td>
+                <td style="text-align:center">{{ $star->education}}</td>
             </tr>
             <tr>
                 <th colspan="6" style="height:10px;"></th>
             </tr>
             <tr>
                 <th>微信账号</th>
-                <td>{{ $star->wechat}}</td>
+                <td style="text-align:center">{{ $star->wechat}}</td>
                 <th>微博账号</th>
-                <td>{{ $star->weibo_id}}</td>
+                <td style="text-align:center">{{ $star->weibo_id}}</td>
                 <th>微拍账号</th>
-                <td>{{ $star->weipai_id}}</td>
+                <td style="text-align:center">{{ $star->weipai_id}}</td>
             </tr>
             <tr>
                 <th>秒拍账号</th>
-                <td>{{ $star->miaopai_id}}</td>
+                <td style="text-align:center">{{ $star->miaopai_id}}</td>
                 <th>美拍账号</th>
-                <td>{{ $star->meipai_id}}</td>
+                <td style="text-align:center">{{ $star->meipai_id}}</td>
                 <th>快手账号</th>
-                <td>{{ $star->kuaishou_id}}</td>
+                <td style="text-align:center">{{ $star->kuaishou_id}}</td>
             </tr>
             <tr>
                 <th colspan="6" style="height:10px;"></th>
             </tr>
             <tr>
                 <th>上衣尺寸</th>
-                <td>{{ $star->shirt_size}}</td>
+                <td style="text-align:center">{{ $star->shirt_size}}</td>
                 <th>下衣尺寸</th>
-                <td>{{ $star->pants_size}}</td>
+                <td style="text-align:center">{{ $star->pants_size}}</td>
                 <th>鞋子尺寸</th>
-                <td>{{ $star->shoes_size}}</td>
+                <td style="text-align:center">{{ $star->shoes_size}}</td>
             </tr>
             <tr>
                 <th colspan="6" style="height:10px;"></th>
             </tr>
             <tr>
-                <th>所在省</th>
-                <td>{{ $star->province}}</td>
-                <th>所在市</th>
-                <td>{{ $star->city}}</td>
-                <th>所在区</th>
+                <th>省</th>
+                <td style="text-align:center">{{ $star->province}}</td>
+                <th>市</th>
+                <td style="text-align:center">{{ $star->city}}</td>
+                <th style="text-align:center">区</th>
                 <td>{{ $star->region}}</td>
             </tr>
             <tr>
@@ -102,7 +102,7 @@
             </tr>
             <tr>
                 <th width="12%">经历：</th>
-                <td colspan="7">{{ $star->experience}}</td>
+                <td colspan="5">{{ $star->experience}}</td>
             </tr>
         </table>
         <a href="javascript:history.back();" style="float: right; margin-right: 40px;"><input type="button" value="返回" class="link_btn"/></a>
@@ -114,7 +114,7 @@
     <div class="admin_tab_cont">
         <div style="margin-left: 20%">
             @foreach ($pictures as $picture)
-            <a href="{{ $picture->url }}" target="_blank"><img src="{{ $picture->url }}" style="width: 80px; height: 80px;"/></a>
+            <a href="{{ $picture->url }}" target="_blank"><img src="{{ $picture->url }}" style="height: 200px;"/></a>
             @endforeach
         </div>
         <a href="javascript:history.back();" style="float: right; margin-top: 20px; margin-right: 20%;"><input type="button" value="返回" class="link_btn"/></a>
