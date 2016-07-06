d081f5dccf6d1b82f081c57c2b772d9ef2efece5
Problemin <problemin@163.com>
2016-07-06 17:52:00 +0800

    添加Model

---
+++ app/Models/TaskResult.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TaskResult extends Model
+{
+    //
+    protected $table = 'task_result';
+
+
+    protected  $primaryKey = 'task_result_id';
+
+    protected  $fillable = [
+        'task_id',
+        'playback_url',
+        'views',
+        'duration'
+    ];
+}
